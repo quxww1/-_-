34985b655d483535a03aff7bc72171ee39f3d275
屈向 <quxww1@gmail.com>
2019-08-08 10:10:10 +0800

    'new'

--- new.js
+++ new.js
@@ -14,6 +14,18 @@ function myNew(fn, ...args) {
     return result instanceof Object ? result : obj; //返回如果result是对象返回 否则返回obj
 }
 
+function myNew2() {
+    var obj = new Object(),
+	// 获得构造函数，arguments中去除第一个参数 Con就是原函数
+    Con = [].shift.call(arguments);
+	// 链接到原型，obj 可以访问到构造函数原型中的属性
+    obj.__proto__ = Con.prototype; //绑定原型
+	// 绑定 this 实现继承，obj 可以访问到构造函数中的属性
+    var ret = Con.apply(obj, arguments);
+	// 优先返回构造函数返回的对象
+	return ret instanceof Object ? ret : obj;
+}
+
 /**
  * 测试
  */
